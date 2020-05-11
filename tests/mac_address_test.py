36dbc142fc014b43fefa4b14fb2d6bfca524804f
Dor Green <dor@armis.com>
2020-05-11 17:47:39 +0300

    Added tests

--- tests/mac_address_test.py
+++ tests/mac_address_test.py
@@ -5,6 +5,22 @@ import pytest
 @pytest.mark.parametrize(["input_string"], [
     ("aa:bb:cc:dd:ee:ff",),
     ("aa-bb-cc-dd-ee-ff",),
+    ("AA-BB-CC-DD-EE-FF",),
+    ("AA:BB:CC:DD:EE:FF",),
 ])
 def test_mac_address_parses_different_strings(input_string):
     assert mac_address.MACAddress(input_string).binary == b"\xaa\xbb\xcc\xdd\xee\xff"
+
+
+def test_mac_address_creation_fails_on_bad_string():
+    with pytest.raises(ValueError):
+        mac_address.MACAddress("foo")
+
+
+def test_string_created_from_binary():
+    assert mac_address.MACAddress(b"\xaa\xbb\xcc\xdd\xee\xff").string == "aa:bb:cc:dd:ee:ff"
+
+
+def test_string_with_hyphen_created_from_binary():
+    assert mac_address.MACAddress(b"\xaa\xbb\xcc\xdd\xee\xff",
+                                  default_mac_separator="-").string == "aa-bb-cc-dd-ee-ff"
